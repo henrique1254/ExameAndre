d852fa1cf89967c1873d5070251206eadc0a1ec0
henrique1254 <henriqueog2000@gmail.com>
2020-12-17 22:07:47 -0300

    terceiro

--- app/src/main/java/com/example/exameandre/MainActivity.java
+++ app/src/main/java/com/example/exameandre/MainActivity.java
@@ -11,23 +11,34 @@ import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView CãoRecy;
-    private EditText ednome;
-    private EditText edraca;
-    private EditText edgenero;
+    private EditText nome;
+    private EditText raca;
+    private EditText genero;
 
     private List<Dog> listacao = new ArrayList<>();
+    private FirebaseDatabase db;
+    DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        nome = findViewById(R.id.name);
+        raca = findViewById(R.id.ra);
+        genero = findViewById(R.id.gen);
+
+        IniciarFirebase();
 
         CãoRecy = findViewById(R.id.listagem);
         criar();
@@ -56,6 +67,25 @@ public class MainActivity extends AppCompatActivity {
 
         Caozinho = new Dog("Cachorro4", "labrador", "masculino");
         this.listacao.add(Caozinho);
+    }
+
+    private void IniciarFirebase()
+    {
+        db = FirebaseDatabase.getInstance();
+        db.setPersistenceEnabled(true);
+        FirebaseApp.initializeApp(this);
+        reference = db.getReference();
+    }
+
+    public void enviar(View view)
+    {
+        Dog Cachorro = new Dog();
+        Cachorro.setNome(nome.getText().toString());
+        Cachorro.setGenero(genero.getText().toString());
+        Cachorro.setRaca(raca.getText().toString());
+
+        reference.child("Dog").child(Cachorro.getNome()).setValue(Cachorro);
+        Toast.makeText(getApplicationContext(),"ENVIADO", Toast.LENGTH_SHORT).show();
     }
 
 
